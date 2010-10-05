ccfd512b402d15dac71e3e58fea1cf2e1f7037b1
Scott Davis <jetviper21@gmail.com>
2010-10-05 12:38:42 -0400

    graphs are in javascript now

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -12,14 +12,14 @@ class ItemController < ApplicationController
     @item = Item.find(params[:id])
     @auctions = @item.auctions.not_hidden.paginate(:page => params[:page], :per_page => 25, :order => 'time DESC')
     @underscored = @item.name.gsub(/\s/, '_')
-    @graph_file = Rails.root.join('public', 'images', 'graph', "#{@underscored}.png")
-    @graph = Gruff::Line.new
-    @graph.theme_keynote
-    @graph.title = @item.name
-    @graph.data(" ", @item.auctions.not_hidden.map(&:price))
-    times = @item.auctions.not_hidden.map {|auc| auc.time.strftime("%a %b %d %I:%M %p")}
-    @graph.labels = {}
-    @graph.write(@graph_file) if @item.auctions.not_hidden.size > 1
+    #@graph_file = Rails.root.join('public', 'images', 'graph', "#{@underscored}.png")
+    #@graph = Gruff::Line.new
+    #@graph.theme_keynote
+    #@graph.title = @item.name
+    #@graph.data(" ", @item.auctions.not_hidden.map(&:price))
+    #times = @item.auctions.not_hidden.map {|auc| auc.time.strftime("%a %b %d %I:%M %p")}
+    #@graph.labels = {}
+    #@graph.write(@graph_file) if @item.auctions.not_hidden.size > 1
   rescue ActiveRecord::RecordNotFound
     flash[:error] = "Item not found"
     redirect_to auction_index_path

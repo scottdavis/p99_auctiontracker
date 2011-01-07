f1710c8e9bdff4f34a3bbcc16ffaeafdec7352e4
Scott Davis <jetviper21@gmail.com>
2011-01-07 04:02:06 -0500

    testing graphs

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -12,7 +12,7 @@ class ItemController < ApplicationController
     expires_in(1.minutes)
     @item = Item.find(params[:id])
     @auctions = @item.auctions.not_hidden.order('time ASC')#.paginate(:page => params[:page], :per_page => 25, :order => 'time DESC')
-    @plot_data = @auctions.map {|a| [a.time.to_i, a.price]}
+    @plot_data= @auctions.map {|a| [a.time.to_s(:rfc822), a.price]}
     @paginate = @item.auctions.not_hidden.order('time DESC').paginate(:page => params[:page], :per_page => 25)
   rescue ActiveRecord::RecordNotFound
     flash[:error] = "Item not found"

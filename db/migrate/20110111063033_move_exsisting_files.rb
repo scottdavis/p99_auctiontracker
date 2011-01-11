6b3c568939d2ba710fe3ca0a7543832535babb6b
Scott Davis <jetviper21@gmail.com>
2011-01-11 01:36:52 -0500

    foo

---
+++ db/migrate/20110111063033_move_exsisting_files.rb
@@ -0,0 +1,15 @@
+class MoveExsistingFiles < ActiveRecord::Migration
+  def self.up
+    Log.all.each do |log|
+      dir = Rails.root.join('public', 'system', 'logs')
+      FileUtils.mkdir_p(dir)
+      next unless File.exists?(log.log)
+      name = File.join(dir, File.basename(log.log))
+      next if name == log.log
+      FileUtils.mv(log.log, name)
+    end
+  end
+
+  def self.down
+  end
+end

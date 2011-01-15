9417cbaadf4ab5583007816a909940ab00901f88
Scott Davis <jetviper21@gmail.com>
2011-01-15 15:21:31 -0500

    cleaned up graphs

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -18,9 +18,14 @@ class ItemController < ApplicationController
     within = (2 * std)
     max = mean + within
     min = mean - within
-    
     @plot_data = @auctions.map do |a|
-      [a.time.to_s(:rfc822), a.price] if a.price.to_f < max.to_f || a.price.to_f > min.to_f
+      if a.price.to_f > max.to_f
+        [a.time.to_s(:rfc822), max]
+      elsif a.price.to_f < min.to_f
+        [a.time.to_s(:rfc822), min]
+      else
+        [a.time.to_s(:rfc822), a.price]
+      end
     end
     @paginate = @item.auctions.not_hidden.order('time DESC').paginate(:page => params[:page], :per_page => 25)
   rescue ActiveRecord::RecordNotFound

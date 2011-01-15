9c2aff7eb24607ec8fa3def0334170f3ec164028
Scott Davis <jetviper21@gmail.com>
2011-01-15 15:10:57 -0500

    cleaned up graphs

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -17,7 +17,7 @@ class ItemController < ApplicationController
     mean = @vector.mean
     within = (2 * std)
     @plot_data = @auctions.map do |a|
-      next if a.price <= (mean + within) || a.price >= (mean - within)
+      next if a.price >= (mean + within) || a.price <= (mean - within)
       [a.time.to_s(:rfc822), a.price]
     end
     @paginate = @item.auctions.not_hidden.order('time DESC').paginate(:page => params[:page], :per_page => 25)

8d7a6e5e9b449f1456cda90faed897774fb16260
Scott Davis <jetviper21@gmail.com>
2011-01-15 14:46:51 -0500

    cleaned up graphs

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -14,7 +14,7 @@ class ItemController < ApplicationController
     @auctions = @item.auctions.not_hidden.order('time ASC')#.paginate(:page => params[:page], :per_page => 25, :order => 'time DESC')
     @vector = @auctions.map(&:price).to_vector(:scale)
     std = @vector.sdp
-    within = 3
+    within = 2
     @plot_data = @auctions.map do |a|
        [a.time.to_s(:rfc822), a.price] if a.price <= std * within || a.price >= (std * within) * -1
      end

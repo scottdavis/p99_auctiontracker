12b714f1cd3c50dc154daaae0e530cdec7bfabd2
Scott Davis <jetviper21@gmail.com>
2011-01-15 19:50:46 -0500

    added dups

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,7 @@
 class LogsController < ApplicationController
   def index
-    @logs = Log.all
-    render :json => @logs.to_json(:only => [:created_at, :processed, :public_path], :methods => :public_path)
+    #@logs = Log.all
+   render :text => 'HA!'
   end
   
 end
